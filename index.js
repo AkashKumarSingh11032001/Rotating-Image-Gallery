a4330f9eb4763bb4a9addc3502d747caef787f5b
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-08-15 10:41:43 +0530

    js func

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const imageContainerEle = document.querySelectorAll(".image-container");
 const prevBtnEle = document.getElementById("prev");
 const nextBtnEle = document.getElementById("next");
+
+let x = 0;
+
+prevBtnEle.addEventListener("click", () => {
+    x = x + 45;
+    updateGallery();
+});
+
+function updateGallery() {
+    imageContainerEle.style.transform = `perspective(1000px) rotateY(${x}deg)`;
+}
